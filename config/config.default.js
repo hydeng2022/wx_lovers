8e5d745235190b3aeb44e3e7fe49c94c5f21d502
hydeng2022 <101695103+hydeng2022@users.noreply.github.com>
2026-03-02 11:02:02 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -41,14 +41,14 @@ module.exports = appInfo => {
   };
 
   config.userData = {
-    mineBirth: "1994-03-24", // 自己的生日
-    gfBirth: "1994-12-26", // 女朋友的生日
-    loveDay: "2007-08-11", // 在一起的日期
-    weatherCity: '茂名市' // 需要获取天气的城市，必须时xx市，xx县，xx自治区, 详细可以去utils/amap.js搜索到就可以，比如广州市，不能是广州
+    mineBirth: "2003-03-17", // 自己的生日
+    gfBirth: "2005-06-14", // 女朋友的生日
+    loveDay: "2024-07-07", // 在一起的日期
+    weatherCity: '榆中县' // 需要获取天气的城市，必须时xx市，xx县，xx自治区, 详细可以去utils/amap.js搜索到就可以，比如广州市，不能是广州
   }
 
   config.userCity = {
-    "adname":"茂名市",
+    "adname":"榆中县",
     "adcode":"440900"
   }
 
